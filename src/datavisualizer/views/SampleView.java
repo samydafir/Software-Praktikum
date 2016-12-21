4985e9b9e6ddf641513a3247fde9ae1dcc5fa7aa
samydafir <samydafir@yahoo.de>
2016-12-21 16:01:21 +0100

    added some test items to view

--- src/datavisualizer/views/SampleView.java
+++ src/datavisualizer/views/SampleView.java
@@ -1,23 +1,28 @@
 package datavisualizer.views;
 
 
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.*;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.nebula.visualization.xygraph.dataprovider.CircularBufferDataProvider;
 import org.eclipse.nebula.visualization.xygraph.figures.IXYGraph;
+import org.eclipse.nebula.visualization.xygraph.figures.ToolbarArmedXYGraph;
 import org.eclipse.nebula.visualization.xygraph.figures.Trace;
 import org.eclipse.nebula.visualization.xygraph.figures.Trace.PointStyle;
 import org.eclipse.nebula.visualization.xygraph.figures.XYGraph;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.draw2d.LightweightSystem;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.*;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.SWT;
 
 
@@ -50,7 +55,10 @@ public class SampleView extends ViewPart {
 	private Action action1;
 	private Action action2;
 	private Action doubleClickAction;
-	private Canvas canvas;
+	private Canvas rightCanvas;
+	private Canvas leftCanvas;
+	ToolbarArmedXYGraph toolbarArmedXYGraph;
+	LightweightSystem lws;
 	 
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
@@ -76,14 +84,33 @@ public class SampleView extends ViewPart {
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
-		canvas = new Canvas(parent, 1);
 		
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		leftCanvas = new Canvas(parent, 1);
+		leftCanvas.setLayout(new RowLayout(1));
+		leftCanvas.setLayoutData(new RowData(500, 500));
+		
+		
+		viewer = new TableViewer(leftCanvas, SWT.CHECK | SWT.H_SCROLL | SWT.V_SCROLL);
 		
 		viewer.setContentProvider(ArrayContentProvider.getInstance());
-		viewer.setInput(new String[] { "One", "Two", "Three" });
+		viewer.setInput(new String[] { "T1","T2","T3","T4","T5","T6","T7","T8","T9","T10","T11","T12","T13", });
 		viewer.setLabelProvider(new ViewLabelProvider());
 
+		rightCanvas = new Canvas(parent, 1);
+		rightCanvas.setLayout(new RowLayout(1));
+		rightCanvas.setLayoutData(new RowData(500, 500));
+		
+		leftCanvas.setLayout(new RowLayout(1));
+		Button button = new Button(leftCanvas, SWT.PUSH);
+		button.setText("Press Me");
+		button.setLayoutData(new RowData(70, 40));
+		button.addListener(SWT.Selection, new Listener() {
+		      public void handleEvent(Event e) {
+		        removeGraph();
+		        System.out.println("Btton pressed");
+		      }
+		    });
+		
 		// Create the help context id for the viewer's control
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "DataVisualizer.viewer");
 		getSite().setSelectionProvider(viewer);
@@ -163,7 +190,7 @@ public class SampleView extends ViewPart {
 	private void hookDoubleClickAction() {
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
 			public void doubleClick(DoubleClickEvent event) {
-				doubleClickAction.run();
+				test();
 			}
 		});
 	}
@@ -182,35 +209,58 @@ public class SampleView extends ViewPart {
 	}
 
 
+	public void removeGraph(){
+		test();
+	}
+	
 	public void test() {
 		
 		// use LightweightSystem to create the bridge between SWT and draw2D
-		final LightweightSystem lws = new LightweightSystem(canvas);
+		lws = new LightweightSystem(rightCanvas);
 
 		// create a new XY Graph.
 		IXYGraph xyGraph = new XYGraph();
-		xyGraph.setTitle("Simple Example");
+		xyGraph.setTitle("Task States");
+		xyGraph.getPrimaryXAxis().setRange(0, 15);
+		xyGraph.getPrimaryYAxis().setRange(0, 10);
+		xyGraph.getPrimaryXAxis().setTitle("time");
+		xyGraph.getPrimaryYAxis().setTitle("State");
+		
+		
+		toolbarArmedXYGraph = new ToolbarArmedXYGraph(xyGraph);;
+		
 		// set it as the content of LightwightSystem
-		lws.setContents(xyGraph);
+		ToolbarArmedXYGraph toolbarArmedXYGraph = new ToolbarArmedXYGraph(xyGraph);
 
+		// set it as the content of LightwightSystem
+		lws.setContents(toolbarArmedXYGraph);
+		
 		// create a trace data provider, which will provide the data to the
 		// trace.
 		CircularBufferDataProvider traceDataProvider = new CircularBufferDataProvider(false);
 		traceDataProvider.setBufferSize(100);
-		traceDataProvider.setCurrentXDataArray(new double[] { 10, 23, 34, 45, 56, 78, 88, 99 });
-		traceDataProvider.setCurrentYDataArray(new double[] { 11, 44, 55, 45, 88, 98, 52, 23 });
-
+		traceDataProvider.setCurrentXDataArray(new double[] {0,1,2,3,3,4,5,6,7,7,8,9,10});
+		traceDataProvider.setCurrentYDataArray(new double[] {1,1,1,1,2,2,2,2,2,3,3,3,3 });
+		
+		CircularBufferDataProvider traceDataProvider1 = new CircularBufferDataProvider(false);
+		traceDataProvider1.setBufferSize(100);
+		traceDataProvider1.setCurrentXDataArray(new double[] {0,1,2,3,3,4,5,6,7,7,8,9,10});
+		traceDataProvider1.setCurrentYDataArray(new double[] {5,5,5,5,7,7,7,7,7,6,6,6,6 });
+		
 		// create the trace
-		Trace trace = new Trace("Trace1-XY Plot", xyGraph.getPrimaryXAxis(), xyGraph.getPrimaryYAxis(), traceDataProvider);
+		Trace trace = new Trace("Task1", xyGraph.getPrimaryXAxis(), xyGraph.getPrimaryYAxis(), traceDataProvider);
+		Trace trace1 = new Trace("Task2", xyGraph.getPrimaryXAxis(), xyGraph.getPrimaryYAxis(), traceDataProvider1);
 
 		// set trace property
-		trace.setPointStyle(PointStyle.XCROSS);
+		trace.setPointStyle(PointStyle.NONE);
+		trace1.setPointStyle(PointStyle.NONE);
 
 		// add the trace to xyGraph
 		xyGraph.addTrace(trace);
+		xyGraph.addTrace(trace1);
 
 		Display display = Display.getDefault();
-		while (!canvas.isDisposed()) {
+		while (!rightCanvas.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
